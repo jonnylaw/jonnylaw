427ed22d1c98214db49c51b27991a617335f56ec
jonnylaw <law.jonny@googlemail.com>
2021-08-17 11:39:53 +0100

    Fix test

--- tests/testthat/test_forward_ad.R
+++ tests/testthat/test_forward_ad.R
@@ -3,11 +3,13 @@ f <- function(x) {
 }
 
 test_that(
-  "Derivative of 5x^2 + 3x + 10",
+  "Derivative of 5x^2 + 3x + 10 is 10a + 3",
   hedgehog::forall(
     list(a = hedgehog::gen.c(hedgehog::gen.element(-100:100))),
     function(a) {
-      expect_equal(object = f(variable(a))$eps, expected = 10 * a + 3)
+      expect_equal(object = f(variable(a))$eps, 
+                   expected = 10 * a + 3, 
+                   tolerance = 1e-3)
     }
   )
 )
